ecddb803d285ffbde940031f00d103576ccb77be
meagan <mfeniseycopes@gmail.com>
2016-12-15 17:32:30 -0500

    Add 11-20

--- 99.js
+++ 99.js
@@ -34,3 +34,34 @@ const pack = list =>
 
 const encode = list =>
   pack(list).map( packing => [packing[0], length(packing)])
+
+const encodeModified = list =>
+  encode(list).map((encoding => encoding[1] === 1 ? encoding[0] : encoding))
+
+const decode = list =>
+  list.reduce(
+    (acc, group) => acc.concat(new Array(group[1]).fill(group[0])),
+    []
+  )
+
+const encodeDirect = list =>
+  list.reduce((acc, el, i) => {
+    el === list[i - 1] ? last(acc)[1] = last(acc)[1] + 1 : acc.push([el, 1])
+    return acc
+  }, [])
+
+const duplicate = list =>
+  list.reduce((acc, el) => acc.concat([el, el]), [])
+
+const duplicateN = (n, list) =>
+  list.reduce((acc, el) => acc.concat(new Array(n).fill(el)), [])
+
+const drop = (n, list) => list.filter((_, i) => (i + 1) % n !== 0)
+
+const split = (n, list) => [list.slice(0, n), list.slice(n)]
+
+const slice = (i, k, list) => list.slice(i, k)
+
+const rotate = (n, list) => flatten(reverse(split(n, list)))
+
+const removeAt = (k, list) => [...slice(0, k, list), ...slice(k + 1, length(list), list)]
